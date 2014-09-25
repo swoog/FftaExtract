b813fa2099431a733f98183613511386743479f0
Aurelien GALTIER <aurelien.galtier@live.fr>
2014-09-25 13:56:53 +0200

    Add ignored files

---
+++ FftaEtract/DatabaseModel/Competition.cs
@@ -0,0 +1,22 @@
+﻿namespace FftaEtract.DatabaseModel
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Competition
+    {
+        [Key]
+        [Column(Order = 1)]
+        public int Year { get; set; }
+
+        public CompetitionInfo CompetitionInfo { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public int CompetitionInfoId { get; set; }
+
+        [Key]
+        [Column(Order = 3)]
+        public CompetitionType Type { get; set; }
+    }
+}
---
+++ FftaEtract/FftaDatabase.cs
@@ -0,0 +1,16 @@
+﻿namespace FftaEtract
+{
+    using System.Collections.Generic;
+    using System.Data.Entity;
+
+    using FftaEtract.DatabaseModel;
+
+    public class FftaDatabase : DbContext
+    {
+        public DbSet<Archer> Archers { get; set; }
+
+        public DbSet<Competition> Competitions { get; set; }
+
+        public DbSet<CompetitionInfo> Clubs { get; set; }
+    }
+}
